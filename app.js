9612b919c09c0e4db8271152557c315ec1775788
Duc Lam <duc.lam@positivethinking.tech>
2022-07-16 01:08:23 +0700

    add question controller

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
 const express = require("express");
-const  db  = require("./config/db");
-const globalErrorHandler = require('./middlewares/globalErrorHandler');
-const authRouter = require('./routes/authRoutes');
-
+const db = require("./config/db");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
+const authRouter = require("./routes/authRoutes");
+const quizController = require("./controllers/quizController");
+const questionController = require("./controllers/questionController");
 
 // Start express app
 const app = express();
 
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true,
-    })
+  express.urlencoded({
+    extended: true,
+  })
 );
 
 // Connect to db
 db.connect();
 
+//Middleware
+app.use((req,res,next)=>{
+    req.user={
+        id:"507f1f77bcf86cd799439011"
+    }
+    next()
+})
+
 // Routers
-app.use('/',authRouter);
+app.use("/", authRouter);
 
+app.post("/api/newquiz", quizController.newQuiz);
+app.get("/api/getQuizzes", quizController.getQuizzes);
+app.get("/api/getquiz/:id", quizController.getQuiz);
+app.put("/api/updatequiz", quizController.updateQuiz);
+app.delete("/api/deletequiz/:id", quizController.deleteQuiz);
 
-app.get("/api/getQuizzes", quizCtrl.getQuizzes);
-app.get("/api/getquiz/:id", quizCtrl.getQuiz);
-app.put("/api/updatequiz", quizCtrl.updateQuiz);
-app.post("/api/newquiz", quizCtrl.newQuiz);
-app.delete("/api/deletequiz/:id", quizCtrl.deleteQuiz);
-
-
-app.get("/api/getquestion/:id", quizCtrl.getQuestion);
-app.get("/api/getquestions/:id", quizCtrl.getQuestions);
-app.put("/api/updatequestion", quizCtrl.updateQuestion);
-app.delete("/api/deletequestion/:id", quizCtrl.deleteQuestion);
-app.post("/api/newquestion", quizCtrl.addQuestion);
-
-
-
-
+app.post("/api/newquestion", questionController.addQuestion);
+app.get("/api/getquestions/:id", questionController.getQuestions);
+app.get("/api/getquestion/:id", questionController.getQuestion);
+app.put("/api/updatequestion", questionController.updateQuestion);
+app.delete("/api/deletequestion/:id", questionController.deleteQuestion);
 
 // Error handler
 app.use(globalErrorHandler);
---
+++ controllers/questionController.js
@@ -0,0 +1,72 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const Quiz = require("../models/quizModel");
+const Question = require("../models/questionModel");
+
+const getQuestions = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  var questions = await Question.find({ quizId: id });
+
+  res.status(200).send(questions);
+});
+
+const addQuestion = catchAsync(async (req, res, next) => {
+  let { id, question, answer1, answer2, answer3, answer4, correctAnswer } =
+    req.body;
+  var newQuestion = new Question({
+    quizId: id,
+    question: question,
+    answer1: answer1,
+    answer2: answer2,
+    answer3: answer3,
+    answer4: answer4,
+    correctAnswer: correctAnswer,
+  });
+  newQuestion
+    .save()
+    .then(() => res.status(200).send())
+    .catch((err) => next(new AppError(err.toString(), 500)));
+});
+
+const deleteQuestion = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  Question.deleteOne({ _id: id })
+
+    .then(() => res.status(200).send())
+    .catch((err) => next(new AppError(err.toString(), 500)));
+});
+
+const getQuestion = catchAsync(async (req, res, next) => {
+  let { id } = req.params;
+  Question.findOne({ _id: id })
+    .then((result) => res.status(200).send(result))
+    .catch((err) => next(new AppError(err.toString(), 500)));
+});
+
+const updateQuestion = catchAsync(async (req, res, next) => {
+  let { id, question, answer1, answer2, answer3, answer4, correctAnswer } =
+    req.body;
+  Question.updateOne(
+    { _id: id },
+    {
+      question: question,
+      answer: answer1,
+      answer2: answer2,
+      answer3: answer3,
+      answer4: answer4,
+      correctAnswer: correctAnswer,
+    }
+  )
+    .then((result) => res.status(200).send(result))
+    .catch((err) => next(new AppError(err.toString(), 500)));
+});
+
+
+
+module.exports = {
+  getQuestions,
+  deleteQuestion,
+  addQuestion,
+  getQuestion,
+  updateQuestion
+};
